b4dd7a91c7c6202686fac851e24451f658de4e5b
Thenoobdude26 <tatendacoolboy@gmail.com>
2025-02-04 12:52:46 +0800

    initial commit

--- C testing/class.c
+++ C testing/class.c
@@ -1,18 +1,43 @@
 #include <stdio.h>
 
 int main() {
-    int count_star;
-    int N;
+    // int count_star;
+    // int N;
 
-    count_star = 0;
+    // count_star = 0;
 
-    printf ("Enter the number of stars: \n");
-    scanf ("%d", &N);
+    // printf ("Enter the number of stars: \n");
+    // scanf ("%d", &N);
 
-    while (count_star<  N){
-        printf ("*");
-        count_star++;
+    // while (count_star<  N){
+    //     printf ("*");
+    //     count_star++;
+// }
+    char answer;
+    char response;
+
+    printf ("Are you a good boy~? (y/n) \n");
+    scanf ("%c", &answer);
+
+    if (answer == 'Y' || answer == 'y'){
+        printf ("That's right you are~ Good boy~\n");
+        printf (
+    "⣿⣿⡇⢩⠘⣴⣿⣥⣤⢦⢁⠄⠉⡄⡇⠛⠛⠛⢛⣭⣾⣿⣿⡏\n"
+    "⣿⣿⣿⡇⠹⢇⡹⣿⣿⣛⣓⣿⡿⠞⠑⣱⠄⢀⣴⣿⣿⣿⣿⡟\n"
+    "⣿⣿⣿⣧⣸⡄⣿⣪⡻⣿⠿⠋⠄⠄⣀⣀⢡⣿⣿⣿⣿⡿⠋\n"
+    "⠘⣿⣿⣿⣿⣷⣭⣓⡽⡆⡄⢀⣤⣾⣿⣿⣿⣿⣿⡿⠋\n"
+    "⠄⢨⡻⡇⣿⢿⣿⣿⣭⡶⣿⣿⣿⣜⢿⡇⡿⠟⠉\n"
+    "⠄⠸⣷⡅⣫⣾⣿⣿⣿⣷⣙⢿⣿⣿⣷⣦⣚⡀\n"
+    "⠄⠄⢉⣾⡟⠙⠶⠖⠈⢻⣿⣷⣅⢻⣿⣿⣿⣿⣿⣶⣶⡆⠄⣤⡀\n"
+    "⠄⢠⣿⣿⣧⣀⣀⣀⣀⣼⣿⣿⣿⡎⢿⣿⣿⣿⣿⣿⣿⣇⠄⠈⠁\n"
+    "⠄⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢇⣎⢿⣿⣿⣿⣿⣿⣿⣿⣶⣶\n"
+    "⠄⠄⠻⢿⣿⣿⣿⣿⣿⣿⣿⢟⣫⣾⣿⣷⡹⣿⣿⣿⣿⣿⣿⣿⡟\n"
+    "⠄⠄⠄⠄⢮⣭⣍⡭⣭⡵⣾⣿⣿⣿⡎⣿⣿⣌⠻⠿⠿⠿⠟⠋\n"
+    "⠄⠄⠄⠄⠈⠻⣿⣿⣿⣿⣹⣿⣿⣿⡇⣿⣿⡿\n"
+    "⠄⠄⣀⣴⣾⣶⡞⣿⣿⣿⣿⣿⣿⣿⣾⣿⡿\n"
+        );
+    } else {
+        printf ("naughty naughty boy~\n");
     }
-    
     return 0;
 }
